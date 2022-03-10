187270fd5b22e31df749d306f672ef7366c5810b
Алина Радюкевич <alina.makaroshka@outlook.com>
2022-03-10 10:25:01 +0300

    Добавьте файлы проекта.

---
+++ _153502_Radyukevich/Program.cs
@@ -0,0 +1,19 @@
+﻿double number1, number2;
+Console.WriteLine("Введите 2 числа:");
+while (!double.TryParse(Console.ReadLine(), out number1)) 
+{
+    Console.WriteLine("Oшибка ввода! Введите число");
+}
+while (!double.TryParse(Console.ReadLine(), out number2)) 
+{
+    Console.WriteLine("Oшибка ввода! Введите число");
+}
+if (number2 == 0)
+{
+    Console.WriteLine("Нельзя разделить на 0");
+}
+else
+{
+    double result = number1 / number2;
+    Console.WriteLine($"Результат деления чисел: {result}");
+}
